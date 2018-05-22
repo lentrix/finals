7cd7abcf26b49f1398539c417e682e9fce8b977f
lentrix <hawkmanlentrix@gmail.com>
2018-05-22 12:40:22 +0800

    Demonstration

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=ws_benj',
+    'username' => 'ws_benj',
+    'password' => 'system32',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
